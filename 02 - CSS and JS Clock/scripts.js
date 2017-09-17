b083fe7717fa01a935eb73f0909b8f51a8e846f0
Ephim <gotsco-en@yandex.ru>
2017-09-17 20:25:47 +0400

    done basics of this tasks

--- 02 - CSS and JS Clock/scripts.js
+++ 02 - CSS and JS Clock/scripts.js
@@ -7,5 +7,32 @@
     const hourHand = document.querySelector('div.hand.hour-hand');
     const minHand = document.querySelector('div.hand.min-hand');
     const secondHand = document.querySelector('div.hand.second-hand');
+    let secondsTransition = null;
+    let timeout = null;
+
+    function transformToDeg() {
+        const time = new Date();
+        let hours = time.getHours();
+        let minutes = time.getMinutes();
+        let seconds = time.getSeconds();
+        const secondsAngle = (seconds * 6) + 90;
+        const minutesAngle = (minutes * 6) + 90;
+        const hourAngle = (30 * hours) + 90;
+        if (seconds === 59) {
+            secondsTransition = secondHand.style.transition;
+            secondHand.style.removeProperty('transition');
+        }
+        if (seconds === 1) {
+            secondHand.style.setProperty('transition', secondsTransition);
+        }
+
+        secondHand.style.setProperty('transform', `rotate(${secondsAngle}deg)`);
+        minHand.style.setProperty('transform', `rotate(${minutesAngle}deg)`);
+        hourHand.style.setProperty('transform', `rotate(${hourAngle}deg)`)
+
+        timeout = setTimeout(transformToDeg, 1000);
+    }
+
+    timeout = setTimeout(transformToDeg, 1000)
 
 })()
