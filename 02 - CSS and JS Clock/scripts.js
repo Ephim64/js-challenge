c3e101227bc6aab145d1ebcbf45956e30d442c88
Ephim <gotsco-en@yandex.ru>
2017-09-17 19:35:57 +0400

    working on script

---
+++ 02 - CSS and JS Clock/scripts.js
@@ -0,0 +1,11 @@
+(function () {
+    'use strict';
+    // setTimeout на 1 секунду
+    // получить текущее время
+    // трансормировать в угол
+    // применить к стрелкам
+    const hourHand = document.querySelector('div.hand.hour-hand');
+    const minHand = document.querySelector('div.hand.min-hand');
+    const secondHand = document.querySelector('div.hand.second-hand');
+
+})()
